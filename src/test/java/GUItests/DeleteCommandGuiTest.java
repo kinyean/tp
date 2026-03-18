e3b99fc0232cf6d4a556921d6e02d5634b55958c
King <kinyean2@gmail.com>
2026-03-18 11:19:12 +0800

    Add test

--- src/test/java/GUItests/DeleteCommandGuiTest.java
+++ src/test/java/GUItests/DeleteCommandGuiTest.java
@@ -1,4 +1,4 @@
-package systemtests;
+package GUItests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -91,6 +91,14 @@ public class JsonAdaptedPersonTest {
     }
 
     @Test
+    public void toModelType_nullWebsite_throwsIllegalValueException() {
+        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                null, VALID_ADDRESS, VALID_TAGS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Website.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
     public void toModelType_invalidWebsite_throwsIllegalValueException() {
         JsonAdaptedPerson person =
                 new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL, INVALID_WEBSITE, VALID_ADDRESS, VALID_TAGS);
